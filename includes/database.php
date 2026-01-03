8b1c5bbd4757ac903a079df892391133dd031090
Thai <thaieaut@gmail.com>
2026-01-04 01:42:45 +0700

    viết hàm truy vấn select dữ liệu

--- includes/database.php
+++ includes/database.php
@@ -2,3 +2,22 @@
 if(!defined('_THAI')){
     die('Error: You do not have permission to access this page.');
 }
+
+// Truy vấn nhiều bản ghi
+function getAll($sql){
+    global $conn;
+    $stm = $conn->prepare($sql);
+    $stm->execute();
+    $result = $stm->fetchAll(PDO::FETCH_ASSOC);
+    return $result;
+}
+
+// Truy vấn một bản ghi
+function getOnce($sql){
+    global $conn;
+    $stm = $conn->prepare($sql);
+    $stm->execute();
+    $result = $stm->fetch(PDO::FETCH_ASSOC);
+    return $result;
+}
+?>
