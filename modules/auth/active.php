64616dcc578ebe63119f8ad4cd869ea35cdd089d
Thai <thaieaut@gmail.com>
2026-01-09 21:03:00 +0700

    Hoàn thiện trang reset mật khẩu và kích hoạt tài khoản

--- modules/auth/active.php
+++ modules/auth/active.php
@@ -2,3 +2,29 @@
 if(!defined('_THAI')){
     die('Error: You do not have permission to access this page.');
 }
+
+require_once 'templates/layouts/header-auth.php';
+?>
+
+<section class="vh-100">
+  <div class="container-fluid h-custom">
+    <div class="row d-flex justify-content-center align-items-center h-100">
+      <div class="col-md-9 col-lg-6 col-xl-5">
+        <img src="<?= HOST_URL_TEMPLATES; ?>/assets/image/draw2.webp"
+          class="img-fluid" alt="Sample image">
+      </div>
+      <div class="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
+        <form>
+          <div class="d-flex flex-row align-items-center justify-content-center justify-content-lg-start">
+            <h2 class="fw-normal mb-5 me-3">Kích hoạt tài khoản thành công</h2>
+          </div>
+          <a href="<?= HOST_URL; ?>?module=auth&action=login"
+                class="link-danger">Đăng nhập ngay</a>
+        </form>
+      </div>
+    </div>
+  </div>
+</section>
+
+<?php
+require_once 'templates/layouts/footer.php';
--- modules/auth/reset.php
+++ modules/auth/reset.php
@@ -2,3 +2,42 @@
 if(!defined('_THAI')){
     die('Error: You do not have permission to access this page.');
 }
+
+require_once 'templates/layouts/header-auth.php';
+?>
+
+<section class="vh-100">
+  <div class="container-fluid h-custom">
+    <div class="row d-flex justify-content-center align-items-center h-100">
+      <div class="col-md-9 col-lg-6 col-xl-5">
+        <img src="<?= HOST_URL_TEMPLATES; ?>/assets/image/draw2.webp"
+          class="img-fluid" alt="Sample image">
+      </div>
+      <div class="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
+        <form>
+          <div class="d-flex flex-row align-items-center justify-content-center justify-content-lg-start">
+            <h2 class="fw-normal mb-5 me-3">Đặt lại mật khẩu</h2>
+          </div>
+
+          <div data-mdb-input-init class="form-outline mb-4">
+            <input type="password" class="form-control form-control-lg"
+              placeholder="Mật khẩu mới" />
+          </div>
+          <div data-mdb-input-init class="form-outline mb-4">
+            <input type="password" class="form-control form-control-lg"
+              placeholder="Nhập lại mật khẩu mới" />
+          </div>
+
+          <div class="text-center text-lg-start mt-4 pt-2">
+            <button  type="button" data-mdb-button-init data-mdb-ripple-init class="btn btn-primary btn-lg"
+              style="padding-left: 2.5rem; padding-right: 2.5rem;">Gửi yêu cầu</button>
+          </div>
+
+        </form>
+      </div>
+    </div>
+  </div>
+</section>
+
+<?php
+require_once 'templates/layouts/footer.php';
